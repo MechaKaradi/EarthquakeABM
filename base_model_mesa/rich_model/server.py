bd2a8bdfd1658ec929c435d7d782e91854fbc36b
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-12-10 14:07:37 +0100

    more tests to see if it works
    refined the tuple expression that is creating the portrayal list in the server.py file

--- base_model_mesa/rich_model/server.py
+++ base_model_mesa/rich_model/server.py
@@ -2,23 +2,42 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule, NetworkModule
 from mesa.visualization.UserParam import UserSettableParameter
 
-from rich_model.model import MinimalModel 
-from rich_model.agents import MinimalAgent
+import math
 
 
-def agent_portrayal(agent):
-    if agent is None:
-        return
-    
-    portrayal = {}
+from .model import MinimalModel
+from .agents import MinimalAgent
 
-    if type(agent) is MinimalAgent:
-        portrayal["Shape"] = "circle"
-        portrayal["color"] = agent.color
-    
+
+def network_portrayal(G):
+    # The model ensures there is 0 or 1 agent per node
+
+    portrayal = dict()
+    portrayal["nodes"] = [
+        {
+            "size": 3,
+            "color": "#CC0000",
+        }
+        for each in list(G.nodes)
+        ]
+
+    portrayal["edges"] = [
+        {
+            "source": source,
+            "target": target,
+            "color": "#000000"
+        }
+        for (source, target) in G.edges()
+    ]
+
     return portrayal
 
-network = NetworkModule(portrayal_method= agent_portrayal)
+model_params = {
+    }
+
+grid = NetworkModule(network_portrayal, 500, 500)
+
+# network = NetworkModule(portrayal_method= agent_portrayal)
 # library='d3'
-server = ModularServer(MinimalModel, [network],"Rich Model Visualisation")
+server = ModularServer(MinimalModel, [grid], "Rich Model Visualisation",model_params)
 server.port = 8521
